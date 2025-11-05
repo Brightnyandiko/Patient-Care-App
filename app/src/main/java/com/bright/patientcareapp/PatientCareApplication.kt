9c6381d68d7aa42771c7a3760cb47228e1953ca8
Brightnyandiko <carlsonbright98@gmail.com>
2025-11-05 19:19:50 +0300

    made an update in the PatientCareApplication.kt file

--- app/src/main/java/com/bright/patientcareapp/PatientCareApplication.kt
+++ app/src/main/java/com/bright/patientcareapp/PatientCareApplication.kt
@@ -3,5 +3,15 @@ package com.bright.patientcareapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Application class for Patient Care App
+ * Enables Hilt dependency injection throughout the app
+ */
 @HiltAndroidApp
-class PatientCareApplication : Application()
+class PatientCareApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Any app-wide initialization can go here
+    }
+}
